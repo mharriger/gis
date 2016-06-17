d55181901219a1b4063e2ae0341949459da18f14
Matt Harriger <mharriger@gmail.com>
2016-06-17 16:45:39 -0500

    Working NED Downloader

--- NEDDownload/NEDDownload.py
+++ NEDDownload/NEDDownload.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import urllib2
 
 if len(sys.argv) < 5:
     raise Exception("Not enough arguments")
@@ -13,4 +14,8 @@ print west, north, east, south
 
 for y in range(south, north + 1):
     for x in range(east, west + 1):
-        print "n{0}w{1}".format(y,x)
+        print "http://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/ArcGrid/n{:02d}w{:03d}.zip".format(y,x)
+        response = urllib2.urlopen("http://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/ArcGrid/n{:02d}w{:03d}.zip".format(y,x))
+        with open('n{0}w{1}.zip'.format(y,x), 'wb') as dest_file:
+            dest_file.write(response.read())
+
